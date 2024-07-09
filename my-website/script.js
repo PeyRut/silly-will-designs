b831554e28e1d17892a3111a7ae115983ac16d44
PeyRut <pruthesell@icloud.com>
2024-07-09 15:28:28 -0500

    Delete my-website directory

--- my-website/script.js
+++
@@ -1,7 +0,0 @@
-function resizeIframe(iframe) {
-    iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Website loaded!");
-});
