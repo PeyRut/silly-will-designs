830ae3fdac31cb090d8a34ae76c3b0cfe11bc4bc
PeyRut <pruthesell@icloud.com>
2024-07-16 11:02:10 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function resizeIframe(iframe) {
-    iframe.style.height = (iframe.contentWindow.document.documentElement.scrollHeight + 20) + 'px'; // Add space for the buffer
+    iframe.style.height = (iframe.contentWindow.document.documentElement.scrollHeight + 50) + 'px'; // Add larger space for the buffer
 }
 
 document.addEventListener("DOMContentLoaded", function() {
